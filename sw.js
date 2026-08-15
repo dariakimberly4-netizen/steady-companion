4937215a9c529487aa5859ccc3605e96ee3cd09a
dariakimberly4-netizen <dariakimberly4@gmail.com>
2026-08-15 08:41:38 +0800

    Refresh cache for ON OFF time tracker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='steady-companion-v4';
+const CACHE='steady-companion-v5';
 const CORE=['./','./index.html','./manifest.webmanifest','./flexible-medicine.js'];
 
 self.addEventListener('install',event=>{
